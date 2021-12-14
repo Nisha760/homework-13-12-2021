35baa3356d387c0d6a0f8ddc0f4362b6f6162db8
Nisha Maurya <nisha.maurya.2@gmail.com>
2021-12-14 09:27:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+console.log("hey")
+
+
+var selectorArr = document.querySelectorAll("header .color-selector")
+var reset = document.querySelector("header .reset")
+
+var circleArr = document.querySelectorAll("main>div")
+
+
+
+for(let i = 0; i < circleArr.length; i++)
+{
+    circleArr[i].innerHTML = i+1 
+    var rndm = Math.floor(Math.random() * 4)
+    circleArr[i].classList.add(`${selectorArr[rndm].classList[1]}`) 
+}
+
+for(let i = 0; i < selectorArr.length; i++)
+{
+    selectorArr[i].addEventListener("click", removeExtra) ;
+}
+
+reset.addEventListener("click", displayAll)
+
+function removeExtra(e)
+{
+    displayAll()
+    console.log(e.currentTarget.className)
+    for(let i = 0; i < circleArr.length; i++)
+    {
+        if((circleArr[i].classList[1]) !== e.currentTarget.classList[1])
+            circleArr[i].style.display = "none"
+    }
+}
+
+function displayAll()
+{
+    for(let i = 0; i < circleArr.length; i++){
+        circleArr[i].style.display = "flex"
+    }
+}
